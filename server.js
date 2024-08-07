2d9a751bc5bf5b0dbbfe38cd00a44408a0a0ef11
Bhaskar magapu <151642780+bhaskar16-github@users.noreply.github.com>
2024-08-07 08:43:03 +0530

    commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require ("http");
+const port= 8081;
+http.createServer((req,res)=>{
+    res.writeHead(200,{"content-Type": "text/html" });
+    res.write("<h1>I am server:-) </br> This is web page</h1>");
+    res.end();
+})
+.listen(port,()=>{
+    console.log(`nodeJs server Started on port:${port}`);
+});
